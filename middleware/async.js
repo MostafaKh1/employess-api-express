e5f692bf58272ae361475734ce187f0b31403fe9
MostafaKh1 <chiiit1754@gmail.com>
2023-07-29 16:24:26 +0300

    edit

--- middleware/async.js
+++ middleware/async.js
@@ -9,15 +9,3 @@ const asyncWrapper = (fn) => {
 };
 
 module.exports = asyncWrapper;
-
-// const asyncWrapper = (fn) => {
-//     return async (req, res, next) => {
-//       try {
-//         await fn(req, res, next);
-//       } catch (error) {
-//         next(error);
-//       }
-//     };
-//   };
-
-//   module.exports = asyncWrapper;
